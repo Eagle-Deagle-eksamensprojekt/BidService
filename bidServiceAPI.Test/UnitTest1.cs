0ef89fdb0ef08079618f0b19ab49e5328d8a22ec
MikkelHv <mikkelh208@gmail.com>
2024-11-27 09:57:33 +0100

    init

---
+++ bidServiceAPI.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace bidServiceAPI.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
