ebae6d18d68ca419c0d4d252b8837d4755b1cec7
N-Edens <128368138+N-Edens@users.noreply.github.com>
2024-12-06 10:57:20 +0100

    Nlog kører måske

--- bidServiceAPI/Program.cs
+++ bidServiceAPI/Program.cs
@@ -1,11 +1,22 @@
+using Microsoft.AspNetCore.Builder;
+using NLog;
+using NLog.Web;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings()
+        .GetCurrentClassLogger();
+        logger.Debug("init main"); // NLog setup
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Registrér at I ønsker at bruge NLOG som logger fremadrettet (før builder.build)
+builder.Logging.ClearProviders();
+builder.Host.UseNLog();
 
 var app = builder.Build();
 
